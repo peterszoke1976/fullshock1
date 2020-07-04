4bac1f1feb6e26de62680bca3d98d1f0d6902fc0
Peti <peterszoke1976@googlemail.com>
2020-07-05 00:27:24 +0200

    Fill data table.

--- js/main.js
+++ js/main.js
@@ -12,9 +12,35 @@ function getServerData(url){
     );
 }
 
-getServerData("http://localhost:3000/users").then(
-        data => console.log(data)
-    );
+document.querySelector("#getDataBtn").addEventListener("click", function() {
+    getServerData("http://localhost:3000/users").then(
+            data => fillDataTable(data, "userTable")
+        );
+});
 
+//Fill table with server data.
+function fillDataTable(data, tableID){
+    let table = document.querySelector(`#${tableID}`);
+    if(!table){
+        console.error(`Table "${tableID}" is not found.`);
+        return;
+    }
+    let tBody = table.querySelector("tbody");
+    for (let row of data){
+        let tr = createAnyElement("tr");
+        for(let k in row){
+            let td = createAnyElement("td");
+            td.innerHTML = row[k];
+            tr.appendChild(td);
+        }
+        tBody.appendChild(tr);
+    }
+}
 
-
+function createAnyElement(name, attributes){
+    let element = document.createElement(name);
+    for (let k in attributes){
+        element.setAttribute(k, attributes[k]);
+    }
+    return element;
+}
